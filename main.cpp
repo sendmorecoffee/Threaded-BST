1c261f0132e6441ff44321763cab9029bf7f7e8e
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2021-12-01 14:00:35 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "ThreadedBST.h"
+
+using namespace std;
+
+// Driver code
+int main()
+{
+    ThreadedBST newTree, *root = NULL;
+    root = newTree.Insert(root, 5);
+    newTree.Insert(root, 3);
+    newTree.Insert(root, 2);
+    newTree.Insert(root, 4);
+    newTree.Insert(root, 7);
+    newTree.Insert(root, 6);
+    newTree.Insert(root, 8);
+ 
+    newTree.Inorder(root);
+    return 0;
+}
