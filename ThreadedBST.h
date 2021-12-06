dcda2ec278ef7eff849e05fdf618476a0f9a5fcb
Jaylon Nelson-Sellers <jcns@uw.edu>
2021-12-05 22:01:11 -0800

    Update ThreadedBST.h

--- ThreadedBST.h
+++ ThreadedBST.h
@@ -1,74 +1,59 @@
 #include <iostream>
 #include <iomanip>
 
-using namespacestd;
-struct tbstNode {
-	int data;
-	tbstNode *left;
-	tbstNode *right;
-	bool rThread;
+template <class ItemType> struct BSTNode
+{
+private:
+  ItemType item; //Value
+  BSTNode <ItemType> *leftChildPtr; //Left Pointer
+  BSTNode <ItemType> *rightChildPtr; //Right Pointer
+  bool isThreadedLeft;	//Bool of left Thread
+  bool isThreadedRight;	//Bool of Right Thread
+
 };
+
+using namespace std;
 
 class ThreadedBST {	
 	friend ostream &operator<<(ostream &out, const SkipList &skip);
-	
-public:
-	tbstNode* Search(int);
-	tbstNode* root;
-	tbstNode* curPtr;
-	
-	// Default constructor
-	ThreadedBST();
 
 	// Parameterized constructor
 	ThreadedBST(int rootItem);
 
-  //Destructor
-  ~ThreadedBST();
-  
-  //Get Depth
-  int getDepth() const;
+	//This is a different method from the one above, This one creates n number of nodes
+	//in nonincremental order
+	ThreadedBST(const int &n);
+
+  	//Destructor
+  	~ThreadedBST();
+
+ 	//Get Depth
+  	int getDepth() const;
 
 	// Insert function
-	ThreadedBST* Insert(ThreadedBST* root, int value);
+	ThreadedBST* Insert(ThreadedBST* root, ItemType value);
 
 	// Completes an inorder traversal of the threaded BST
 	void Inorder(ThreadedBST* root);
-	
-	// Delete
 
-	void remove(tbst<ItemType> *node, ItemType data);
-	
+	// Remove
+	void remove(tbst<ItemType> *node, ItemType value);
+
 	// Copy
 	ThreadedBST(const ThreadedBST<ItemType> &tree);
-	
+
 	// Remove Even
 	void removeEven();
 	
-  //Inorder Traverse for testing
-  void inorderTraverse();
-  
-  //I don't think we need these. 
-	// Operator= overload
-	// Operator<< overload
-	
+	//We'll have to pick between these two, I have no preferance but if we're in the Tree we have access to the root already.
+
+ 	//Inorder Traverse for testing
+ 	void inorderTraverse();
+
+	// Completes an inorder traversal of the threaded BST
+	void Inorder(ThreadedBST* root);
 
 	// Clear
-	void clear()
+	void clear();
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
