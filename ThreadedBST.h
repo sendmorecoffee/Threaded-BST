a5cdd2f6855709b089c6cc5e5a904892b9540fbe
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2021-12-05 22:16:01 -0800

    Update ThreadedBST.h

--- ThreadedBST.h
+++ ThreadedBST.h
@@ -33,9 +33,6 @@ class ThreadedBST {
 	// Insert function
 	ThreadedBST* Insert(ThreadedBST* root, ItemType value);
 
-	// Completes an inorder traversal of the threaded BST
-	void Inorder(ThreadedBST* root);
-
 	// Remove
 	void remove(tbst<ItemType> *node, ItemType value);
 
@@ -44,14 +41,9 @@ class ThreadedBST {
 
 	// Remove Even
 	void removeEven();
-	
-	//We'll have to pick between these two, I have no preferance but if we're in the Tree we have access to the root already.
 
- 	//Inorder Traverse for testing
- 	void inorderTraverse();
-
-	// Completes an inorder traversal of the threaded BST
-	void Inorder(ThreadedBST* root);
+ 	// Completes an inorder traversal of the threaded BST
+ 	void Inorder();
 
 	// Clear
 	void clear();
