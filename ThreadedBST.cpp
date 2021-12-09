07dd23b7ba5a9bdd59565f5af7eaf05a3e799fe4
BlakeD327 <47874373+BlakeD327@users.noreply.github.com>
2021-12-09 09:01:38 -0800

    Update ThreadedBST.cpp

--- ThreadedBST.cpp
+++ ThreadedBST.cpp
@@ -24,15 +24,15 @@ ThreadedBST* ThreadedBST::Insert(ThreadedBST* root, int value) {
 	//Insert data
 	if(value > root->data) {
 		// Insert right node data, if the 'value'
-    // to be inserted is greater than 'root' node data.
+    		// to be inserted is greater than 'root' node data.
  
-    // Process right nodes.
+    		// Process right nodes.
 		root->right = Insert(root->right, value);
 	} else {
 		// Insert left node data, if the 'value'
-    // to be inserted is greater than 'root' node data.
+    		// to be inserted is greater than 'root' node data.
  
-    // Process left nodes.
+    		// Process left nodes.
 		root->left = Insert(root->left, value);
 	}
 
